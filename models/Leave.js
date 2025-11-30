9831c2d3eb453a4dfeb9b497feaef0c5fd100254
sridhar-hp <sridhark3773@gmail.com>
2025-11-30 16:32:00 +0530

    done

--- models/Leave.js
+++ models/Leave.js
@@ -1,3 +1,18 @@
+// const mongoose = require("mongoose");
+
+// const leaveSchema = new mongoose.Schema({
+//     userId: { type: String, required: true }, 
+//     role: { type: String, required: true },
+//     name: { type: String, required: true },
+//     leaveType: { type: String, required: true },
+//     startDate: { type: Date, required: true },
+//     endDate: { type: Date, required: true },
+//     leaveReason: { type: String, required: true },
+//     appliedDate: { type: Date, default: Date.now },
+//     status: { type: String, default: "Pending" }
+// });
+
+// module.exports = mongoose.model("leaves", leaveSchema);
 const mongoose = require("mongoose");
 
 const leaveSchema = new mongoose.Schema({
