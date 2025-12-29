ed02c60d9586f801daf45de90555c6b8f637dc56
sridhar-hp <sridhark3773@gmail.com>
2025-12-29 19:55:19 +0530

    done

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const {apply} = require("../controllers/applyController");
-const {status}= require("../controllers/statusController");
-const {getLeaveBalance}=require("../controllers/leaveController")
+const { apply } = require("../controllers/applyController");
+const { status } = require("../controllers/statusController");
+const { getLeaveBalance } = require("../controllers/leaveController")
 
-router.post("/sapply",apply);
-router.get("/status/:userId",status);
+router.post("/sapply", apply);
+router.get("/status/:userId", status);
 router.get("/leave-balance/:id", getLeaveBalance);
 
 module.exports = router;
