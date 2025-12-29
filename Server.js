c1a5ecc293a0889f0ae9c83d006790d467d268c1
sridhar-hp <sridhark3773@gmail.com>
2025-12-29 19:59:08 +0530

    done

--- Server.js
+++ Server.js
@@ -11,7 +11,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true
