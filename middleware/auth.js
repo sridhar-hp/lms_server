53eb46d77681bd96ca591e74eecbbdc0cccd40d6
sridhar-hp <sridhark3773@gmail.com>
2025-12-29 19:50:25 +0530

    done

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,16 +1,16 @@
 const jwt = require("jsonwebtoken");
 //no use 
 module.exports = (req, res, next) => {
-  const token = req.cookies.token;
+    const token = req.cookies.token;
 
-  if (!token)
-    return res.status(401).json({ success: false, message: "No token, unauthorized" });
+    if (!token)
+        return res.status(401).json({ success: false, message: "No token, unauthorized" });
 
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
-    next();
-  } catch (err) {
-    res.status(401).json({ success: false, message: "Invalid token" });
-  }
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (err) {
+        res.status(401).json({ success: false, message: "Invalid token" });
+    }
 };
