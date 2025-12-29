d7f8b2babd88829f419d02cbb53e5fc992dd4355
sridhar-hp <sridhark3773@gmail.com>
2025-12-29 19:54:40 +0530

    done

--- models/User.js
+++ models/User.js
@@ -1,19 +1,19 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  Id: { type: String, required: true, unique: true },
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  role: { type: String, default: null },
-  leaveBalance: {
-    type: Object,
-    default: {
-      "Annual Leave": 14,
-      "Sick Leave": 8,
-      "Casual Leave": 6
+    Id: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, default: null },
+    leaveBalance: {
+        type: Object,
+        default: {
+            "Annual Leave": 14,
+            "Sick Leave": 8,
+            "Casual Leave": 6
+        }
     }
-  }
 
 });
 
