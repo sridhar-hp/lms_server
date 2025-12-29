a39b9feb71d9eafd4b0c7e82ca42cb744af0ddb7
sridhar-hp <sridhark3773@gmail.com>
2025-12-29 19:49:52 +0530

    done

--- middleware/auth.js
+++ middleware/auth.js
@@ -8,7 +8,7 @@ module.exports = (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded; 
+    req.user = decoded;
     next();
   } catch (err) {
     res.status(401).json({ success: false, message: "Invalid token" });
