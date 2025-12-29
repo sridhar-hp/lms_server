077c6cb2016993feba066ac5c11b21d8f4a04d8d
sridhar-hp <sridhark3773@gmail.com>
2025-12-29 19:52:29 +0530

    done

--- controllers/settingController.js
+++ controllers/settingController.js
@@ -1,36 +1,34 @@
 const user = require("../models/User");
 const leave = require("../models/Leave");
 
-const setting = async(req ,res)=>{
-    try{
-        const users = await user.find();
-        return res.json({success: true , message:"all user list",users});
-    }
-    catch(err)
-    {
-        console.log(err);
-    }
+const setting = async (req, res) => {
+  try {
+    const users = await user.find();
+    return res.json({ success: true, message: "all user list", users });
+  }
+  catch (err) {
+    console.log(err);
+  }
 };
 
-const users= async(req,res)=>{
-    try{
-        const {id}=req.params;
-        const updatedData= req.body;
-        const ssave = await user.findByIdAndUpdate(
-            id,
-            updatedData,
-            {new:true}            
-        );
-        res.json({
-            success:true,
-            message:"user updated successfully",
-            ssave
-        });
-    }
-    catch(err)
-    {
-        console.log(err);
-    }
+const users = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedData = req.body;
+    const ssave = await user.findByIdAndUpdate(
+      id,
+      updatedData,
+      { new: true }
+    );
+    res.json({
+      success: true,
+      message: "user updated successfully",
+      ssave
+    });
+  }
+  catch (err) {
+    console.log(err);
+  }
 };
 
 const deleteUser = async (req, res) => {
@@ -57,4 +55,4 @@ const deleteUser = async (req, res) => {
   }
 };
 
-module.exports={setting,users,deleteUser};
+module.exports = { setting, users, deleteUser };
