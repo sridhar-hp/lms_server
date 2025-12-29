862a9c86470ab2700f08e1e4c458949d9326c9a5
sridhar-hp <sridhark3773@gmail.com>
2025-12-29 19:52:10 +0530

    done

--- controllers/leaveController.js
+++ controllers/leaveController.js
@@ -1,22 +1,22 @@
 const User = require("../models/User");
 
 const getLeaveBalance = async (req, res) => {
-  try {
-    const user = await User.findOne({ Id: req.params.id });
+    try {
+        const user = await User.findOne({ Id: req.params.id });
 
-    if (!user) {
-      return res.status(404).json({ success: false, message: "User not found" });
+        if (!user) {
+            return res.status(404).json({ success: false, message: "User not found" });
+        }
+
+        res.json({
+            success: true,
+            leaveBalance: user.leaveBalance
+        });
+
+    } catch (err) {
+        console.log("ERROR:", err);
+        res.status(500).json({ success: false });
     }
-
-    res.json({
-      success: true,
-      leaveBalance: user.leaveBalance
-    });
-
-  } catch (err) {
-    console.log("ERROR:", err);
-    res.status(500).json({ success: false });
-  }
 };
 
 module.exports = { getLeaveBalance };
