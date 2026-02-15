80b073f8ce2994584690726967b429424bce8716
sridhar-hp <sridhark3773@gmail.com>
2026-02-15 13:07:21 +0530

    75% jwt authentication is done logout,loginstate and setting is pending

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,21 +1,31 @@
 import jwt from "jsonwebtoken";
+import User from "../models/User.js";
 
-export const protect = (req, res, next)=>{
-    
+const protect = async (req, res, next) => {
+    let token;
+
     const authHeader = req.headers.authorization;
 
-    if(!authHeader || !authHeader.startsWith("Bearer ")) {
-        return res.status(401).json({message: "No taken, access denied"});
-    }
+    if (authHeader && authHeader.startsWith("Bearer ")) {
+        try {
+            token = authHeader.split(" ")[1];
 
-    const token = authHeader.split(" ")[1];
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    try{
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded; 
-        next();
-    }
-    catch(err){
-        return res.status(401).json({message: "Invalid or expired token"});
+            req.user = await User.findOne({ Id: decoded.Id }).select("-password");
+
+            if (!req.user) {
+                return res.status(401).json({ message: "User not found" });
+            }
+
+            next(); // ✅ allow request
+        } catch (error) {
+            console.log("JWT ERROR:", error);
+            return res.status(401).json({ message: "Not authorized, token failed" });
+        }
+    } else {
+        return res.status(401).json({ message: "Not authorized, no token" });
     }
 };
+
+export { protect };
