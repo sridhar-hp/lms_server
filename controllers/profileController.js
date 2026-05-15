1fe8b174d976c12f9c80985a5830f428a197c955
sridhar-hp <sridhark3773@gmail.com>
2026-05-15 16:55:29 +0530

    bug fix

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,38 +1,55 @@
 import User from "../models/User.js";
 
 export const getProfile = async (req, res) => {
-    const userId= req.params.userId;
-    try{
-        const user = await User.findOne({Id:userId});
-        if(!user){
+    const userId = req.params.userId;
+    try {
+        const user = await User.findOne({ Id: userId });
+        if (!user) {
             return res.status(404).json({ success: false, message: "User not found" });
         }
         res.json({
             success: true,
             profiledetails: user
-        }); 
+        });
         console.log("this is profile :", user);
 
     }
-    catch(err){
+    catch (err) {
         res.status(500).json({ success: false, message: "Server error" });
     }
 };
 
 export const updateProfile = async (req, res) => {
-    const userId= req.params.userId;
+    const userId = req.params.userId;
     const { name, email, phoneNumber } = req.body;
 
-    try{
+    try {
         const userUpdate = await User.findOneAndUpdate(
             { Id: userId },
             { name, email, phoneNumber },
-            { new: true }
-        );
-        res.status(200).json({ success: true, message: "Profile updated successfully", updatedProfile: userUpdate });
+            {
+                new: true,
+                runValidators: true
+            });
+
+        if (!userUpdate) {
+            return res.status(404).json({
+                success: false,
+                message: "User not found"
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "Profile updated successfully",
+            updatedProfile: userUpdate,
+        });
     }
-    catch(err){
+    catch (err) {
         console.log("Error updating profile:", err);
-        res.status(500).json({ success: false, message: "Server error" });
+        res.status(500).json({
+            success: false,
+            message: "Server error"
+        });
     }
 };
